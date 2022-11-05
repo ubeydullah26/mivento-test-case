2547f9dfbac7d393b2d990d2c20f8f682a5eb993
Ubeydullah Yılmaz <ubey.yilmaz44@gmail.com>
2022-11-06 01:25:47 +0300

    assessment done

---
+++ config/database.php
@@ -0,0 +1,43 @@
+<?php
+
+//create database
+try{
+    $pdo = new PDO("mysql:host=localhost", "root", "");
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS `mivento` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+    $pdo->exec("USE `mivento`");
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
+
+
+//Database Connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mivento";
+
+// Create connection
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// set the PDO error mode to exception
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//create tables
+try {
+    $pdo->exec("CREATE TABLE IF NOT EXISTS campaigns (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        date VARCHAR(255) NOT NULL
+    )");
+    $pdo->exec("CREATE TABLE IF NOT EXISTS employees (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        campaign_id INT(11),
+        name VARCHAR(255) NOT NULL,
+        surname VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        employee_id VARCHAR(12) NOT NULL,
+        phone VARCHAR(10) NOT NULL,
+        point VARCHAR(255) NOT NULL
+    )");
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
--- import.php
+++ import.php
@@ -1,26 +1,42 @@
 <?php
 
-//Database Connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mivento";
+//database connection
+include_once 'config/connection.php';
 
-// Create connection
-$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-// set the PDO error mode to exception
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//validation
+if(empty($_POST['name']) || empty($_POST['date'])){
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Lütfen zorunlu alanları doldurunuz.'
+    ]);
+    exit;
+}
+
+//insert campaigns
+try{
+    //insert query
+    $stmt = $pdo->prepare("INSERT INTO campaigns (name, date) VALUES (?, ?)");
+    $stmt->execute([
+        $_POST['name'],
+        $_POST['date']
+    ]);
+//get last inserted id
+    $last_id = $pdo->lastInsertId();
+}catch (PDOException $e){
+    echo json_encode([
+        "status" => "error",
+        "message" => $e->getMessage()
+    ]);
+}
 
 //Get the file
-ini_set('auto_detect_line_endings',TRUE);
+ini_set('auto_detect_line_endings', TRUE);
 $fh = fopen($_FILES['csv']['tmp_name'], 'r');
 if (!$fh) {
     die("Dosya açılamadı");
 }
 
-
-//Import row by row
-$stored = [];
+//Import csv data
 $flag = true;
 while (($row = fgetcsv($fh, 0, ';')) !== FALSE) {
     //Skip the first row
@@ -29,7 +45,8 @@ while (($row = fgetcsv($fh, 0, ';')) !== FALSE) {
         continue;
     }
 
-    $stored[] = [
+    //rename
+    $row = [
         "name" => $row[0],
         "surname" => $row[1],
         "email" => $row[2],
@@ -38,45 +55,56 @@ while (($row = fgetcsv($fh, 0, ';')) !== FALSE) {
         "point" => $row[5],
     ];
 
+    //validate email if not valid skip
+    if (!filter_var($row["email"], FILTER_VALIDATE_EMAIL)) {
+        continue;
+    }
+    //phone validation if not valid replace format
+    if (!preg_match('/^[5][0-9]{9}$/', $row["phone"])) {
+        //purge special characters
+        $row["phone"] = preg_replace('/[^0-9]/', '', $row["phone"]);
+        //remove 0 from the beginning
+        $row["phone"] = ltrim($row["phone"], '0');
+    }
+
     try {
-//        $stmt = $pdo->prepare("INSERT INTO employees (`name`, `surname`, `email`, `employee_id`, `phone`, `point`) VALUES (?, ?, ?, ?, ?, ?)");
-//        $stmt->execute([
-//            $row[0],
-//            $row[1],
-//            $row[2],
-//            $row[3],
-//            $row[4],
-//            $row[5]
-//        ]);
-    } catch (Exception $e) {
-        echo $e->getMessage();
+        //insert all data
+        $stmt = $pdo->prepare("INSERT INTO employees (`campaign_id`, `name`, `surname`, `email`, `employee_id`, `phone`, `point`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([
+            $last_id,
+            $row["name"],
+            $row["surname"],
+            $row["email"],
+            $row["employee_id"],
+            $row["phone"],
+            $row["point"]
+        ]);
+
+        //remove duplicate emails to database
+        $stmt = $pdo->prepare("DELETE FROM employees WHERE email IN (SELECT a.email FROM (
+            SELECT COUNT(*) AS cnt, email FROM employees GROUP BY email HAVING cnt>1) AS a);");
+        $stmt->execute();
+        //remove duplicate employee_id to database
+        $stmt = $pdo->prepare("DELETE FROM employees WHERE employee_id IN (SELECT a.employee_id FROM (
+            SELECT COUNT(*) AS cnt, employee_id FROM employees GROUP BY employee_id HAVING cnt>1) AS a);");
+        $stmt->execute();
+        //remove duplicate phone to database
+        $stmt = $pdo->prepare("DELETE FROM employees WHERE phone IN (SELECT a.phone FROM (
+            SELECT COUNT(*) AS cnt, phone FROM employees GROUP BY phone HAVING cnt>1) AS a);");
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo json_encode([
+            "status" => "error",
+            "message" => $e->getMessage()
+        ]);
     }
 }
-
-
-function unique_multidim_array($array, $key)
-{
-    $i = 0;
-    $temp_array = [];
-    $key_array = [];
-
-    foreach ($array as $val) {
-        if (!in_array($val[$key], $key_array)) {
-            $key_array[$i] = $val[$key];
-            $temp_array[$i] = $val;
-        }
-        $i++;
-    }
-    return $temp_array;
-}
-
-$unique_field = ['email', 'employee_id', 'phone'];
-foreach ($unique_field as $field) {
-    $stored = unique_multidim_array($stored, $field);
-}
-print_r($stored);
-
 
 //Close the file
-ini_set('auto_detect_line_endings',FALSE);
+ini_set('auto_detect_line_endings', FALSE);
 fclose($fh);
+
+echo json_encode([
+    "status" => "success",
+    "message" => "Veriler başarıyla eklendi"
+]);
